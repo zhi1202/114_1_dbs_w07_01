d5fa54297168b9a395b5cc341d00293f239529c2
zhi1202 <C112181107@nkust.edu.tw>
2025-10-23 13:03:15 +0800

    hello

---
+++ 1023.php
@@ -0,0 +1,12 @@
+<?php
+$arr = array(
+    "sid" => "C112181107",
+    "name" => "zhi",
+    "shape" => "Triangle",
+    100
+);
+
+// foreach ($arr as $key => $vaule) {
+//     echo "$key => $value";
+// };
+foreach ($arr as $key => $value) echo "$key => $value <BR> ";
